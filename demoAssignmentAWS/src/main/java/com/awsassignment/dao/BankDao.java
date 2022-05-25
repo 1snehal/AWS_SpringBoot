dee4701afda09352bd2103e853dea53468848cec
1snehal <78212185+1snehal@users.noreply.github.com>
2022-05-25 19:39:10 +0530

    Delete demoAssignmentAWS directory

--- demoAssignmentAWS/src/main/java/com/awsassignment/dao/BankDao.java
+++
@@ -1,7 +0,0 @@
-package com.awsassignment.dao;
-import org.springframework.stereotype.Service;
-
-import com.awsassignment.pojo.Bank;
-public interface BankDao {
-	public Bank saveBankdatails(Bank bank);
-}
--- demoAssignmentAWS/src/main/java/com/awsassignment/repository/BankRepository.java
+++
@@ -1,8 +0,0 @@
-package com.awsassignment.repository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.awsassignment.pojo.Bank;
-@Repository
-public interface BankRepository extends CrudRepository<Bank, Integer>{
-
-}
