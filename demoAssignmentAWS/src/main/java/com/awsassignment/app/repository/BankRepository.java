babcfdc98428edf0d1dfcfbdeda899b6f17fdcc9
1snehal <78212185+1snehal@users.noreply.github.com>
2022-06-03 15:03:15 +0530

    Delete demoAssignmentAWS/src/main/java/com/awsassignment/app directory

--- demoAssignmentAWS/src/main/java/com/awsassignment/app/repository/BankRepository.java
+++
@@ -1,9 +0,0 @@
-package com.awsassignment.app.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.awsassignment.app.pojo.Bank;
-@Repository
-public interface BankRepository extends JpaRepository<Bank, Integer>{
-
-}
