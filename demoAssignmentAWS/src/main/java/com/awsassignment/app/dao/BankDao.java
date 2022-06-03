0613e4b542d252036fe7424606f900a7b0363d5d
1snehal <78212185+1snehal@users.noreply.github.com>
2022-06-03 15:06:51 +0530

    Delete demoAssignmentAWS/src/main/java/com/awsassignment/app directory

--- demoAssignmentAWS/src/main/java/com/awsassignment/app/dao/BankDao.java
+++
@@ -1,8 +0,0 @@
-package com.awsassignment.app.dao;
-import org.springframework.stereotype.Repository;
-
-import com.awsassignment.pojo.Bank;
-@Repository
-public interface BankDao {
-	Bank saveBankdatails(Bank banklist);
-}
